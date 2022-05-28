c9bdac9ccb2afd762a24a2826549db3c4c004e2b
t0uu <srtouzet@hotmail.com>
2022-05-28 14:59:06 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import './style.css'
+
+
+
+// const App = document.getElementById('App') as HTMLElement;
+const containerChart = document.getElementById('containerChart') as HTMLElement
+const getData = async() =>  {
+        const response = await fetch('/data.json');
+        const data:[] = await response.json();
+        
+        
+         data.forEach(element => {
+         const {day,amount} = element
+             
+         containerChart.innerHTML += `
+         <div class="containerBars">
+         <div class="numbers" id="amount">$${amount}</div>
+         <div class="barChart"  style="height:${amount * 3.1}px"></div>
+         <div><small class="textDay">${day}</small></div>
+         </div>
+         `
+         });  
+    }
+
+
+
+getData()
